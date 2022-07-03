383e7b78ec7aeba5a78a1330a722c76ca44a372b
Haley Telega <haleytelega@outlook.com>
2022-07-03 14:31:22 -0400

    working on header and nav

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+    <header>
+    <h2>
+        <a data-testid="link" href="/">
+            <p>Haley Telega's Portfolio</p>
+        </a>
+    </h2>
+    </header>
+    );
+}
+
+export default Header;
