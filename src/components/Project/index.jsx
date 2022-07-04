b5116c45c1afbbc6616e543ffe2a3d47c55799b2
Haley Telega <haleytelega@outlook.com>
2022-07-03 21:39:01 -0400

    trying to get the photos to go big

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 const ProjectList = ({ category }) => {
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    // const [currentPhoto, setCurrentPhoto] = useState();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentPhoto, setCurrentPhoto] = useState();
 
     const [photos] = useState([
     {
@@ -33,19 +34,23 @@ const ProjectList = ({ category }) => {
 
     const currentPhotos = photos.filter(photo => photo.category === category);
 
-    // const toggleModal = (image, i) => {
-    //     setCurrentPhoto({ ...image, index: i });
-    //     setIsModalOpen(!isModalOpen);
-    // };
+    const toggleModal = (image, i) => {
+        setCurrentPhoto({ ...image, index: i });
+        setIsModalOpen(!isModalOpen);
+    };
 
     return (
         <div>
+            {isModalOpen && (
+            <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+        )}
             <div className="flex-row">
                 {currentPhotos.map((image, i) => (
                 <img style={{ width: "20%" }}
                 src={require(`../../assets/${category}/${i}.jpg`).default}
                 alt={image.name}
                 className="img-thumbnail mx-1"
+                onClick={() => toggleModal(image, i)}
                 key={image.name}
                 />
             ))}
