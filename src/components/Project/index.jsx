7652bfbf1f19df9ce66ddc4d39866b14bf13bac4
Haley Telega <haleytelega@outlook.com>
2022-07-04 13:59:10 -0400

    trying to get text to show up over image when hovering

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -4,7 +4,8 @@ const ProjectList = ({ category }) => {
     const [photos] = useState([
     {
         name: 'Weather Dashboard',
-        category: 'portfolio'
+        category: 'portfolio',
+        description: 'Weather Dashboard'
     },
     {
         name: 'Workday Scheduler',
@@ -30,13 +31,29 @@ const ProjectList = ({ category }) => {
 
     const currentPhotos = photos.filter(photo => photo.category === category);
 
+    const [hover, setHover] = useState(false); // initial false
+
+
+    const onHover = (e) => {
+    e.preventDefault();
+    setHover(true); // turn true
+    console.log("hovered");
+    };
+
+    const onHoverOver = (e) => {
+    e.preventDefault(); // turn false
+    setHover(false);
+    };
 
     return (
-            <div>
+        <div>
             <div className="flex-row">
+            {hover && <p className={hover}>hovered</p>}
                 {currentPhotos.map((image, i) => (
                 <img style={{ width: "20%" }}
                 src={require(`../../assets/${category}/${i}.jpg`).default}
+                onMouseEnter={(e) => onHover(e)}
+                onMouseLeave={(e) => onHoverOver(e)}
                 alt={image.name}
                 className="img-thumbnail mx-1"
                 key={image.name}
