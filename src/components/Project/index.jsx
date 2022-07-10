f21b30f9f83cf6e7926d4ab53ea80430e115a6cd
Haley Telega <haleytelega@outlook.com>
2022-07-10 16:07:35 -0400

    trying to get link to work

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -2,49 +2,48 @@
 import React, { useState } from 'react';
 import ProjectNames from '../ProjectNames';
 
-const ProjectList = ({ category, repo }) => {
+const ProjectList = ({ category, link }) => {
     const [photos] = useState([
     {
         name: 'Weather Dashboard',
         category: 'portfolio',
-        repo: 'https://github.com/haleytelega/Weather-Dashboard',
         link: 'https://haleytelega.github.io/Weather-Dashboard/'
     },
     {
         name: 'Workday Scheduler',
         category: 'portfolio',
-        repo:'https://github.com/haleytelega/Work-Day-Scheduler-'
+        link:'https://haleytelega.github.io/Work-Day-Scheduler-/'
     },
     {
         name: 'Password Generator',
         category: 'portfolio',
-        repo: 'https://github.com/haleytelega/Password-Generator'
+        link: 'https://haleytelega.github.io/Password-Generator/'
     },
     {
         name: 'Note Taker',
         category: 'portfolio',
-        repo: 'https://github.com/haleytelega/Note-Taker'
+        link: 'https://ht-note-taker.herokuapp.com/'
     },
     {
         name: 'Employee Tracker',
         category: 'portfolio',
-        repo:'https://github.com/haleytelega/Employee-Tracker'
+        link:'https://github.com/haleytelega/Employee-Tracker'
     },
     {
         name: 'Tech Blog',
         category: 'portfolio',
-        repo:'https://github.com/haleytelega/Tech-Blog'
+        link:'https://haleys-tech-blog.herokuapp.com/'
     }
     ]);
 
-    const currentPhotos = photos.filter(photo => photo.category === category && photo.repo === repo);
-    
+    const currentPhotos = photos.filter(photo => photo.category === category && photo.link === link);
+
     return (
         <div>
             <div className="flex-row">
                 {currentPhotos.map((image, i ) => (
                     <ProjectNames key={image.name} 
-                    img={image} i={i} category={category} repo={photos[i].repo}  / >
+                    img={image} i={i} category={category} repo={photos[i].link}  / >
             ))}
             </div>
         </div>
--- src/components/ProjectNames/index.jsx
+++ src/components/ProjectNames/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const ProjectNames = ({ img, i, category, repo }) => {
+const ProjectNames = ({ img, i, category, link }) => {
     
     return (
         <>
         <div className="portfolio_container">
             <div className="portfolio_container-info">
-                <a className="portfolio_container-name" href={repo} target="_blank" rel="noreferrer">{img.name}</a>
+                <a className="portfolio_container-name" href={link} target="_blank" rel="noreferrer">{img.name}</a>
             </div>
             <img
                 src={require(`../../assets/${category}/${i}.jpg`).default}
