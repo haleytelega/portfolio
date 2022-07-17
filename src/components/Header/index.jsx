856a66d8c999eb03fb139e98e616d0dea2e9cac4
Haley Telega <haleytelega@outlook.com>
2022-07-17 18:56:17 -0400

    fixed homepage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,12 +7,12 @@ function Header() {
     <header className="mb-4 py-2 flex-row align-center">
         <div className="container flex-row justify-space-between-lg justify-center align-center">
         <h2>
-            <a href="/About">
+            <a href="/">
                 <h4 className="header">Haley Telega's Portfolio</h4>
             </a>
         </h2>
         <nav>
-            <Link to="/About"> About Me</Link>
+            <Link to="/"> About Me</Link>
             <Link to="/Portfolio"> Portfolio</Link>
             <a className="resume" href={pdf} target="_blank" rel="noreferrer">
             Resume
