b5deab3e0c54df4d5fa574967fcebefcc222cf1d
Haley Telega <haleytelega@outlook.com>
2022-08-02 11:47:11 -0400

    made more changes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Footer () {
     return (
     <footer>
-    <div>
+    <Row>
+    <Col>
         <div className="footer">
         <a href="https://github.com/haleytelega?page=1&tab=repositories" target="_blank" rel="noreferrer">
-            <img style={{ width: "5%" }}
+            <img style={{ width: "10%" }}
             src={require(`../../assets/footer/0.jpg`).default}
             alt={'GitHub logo'}
-            className="footer-img"
+            className="footer-img1"
             key={'GitHub logo'}
             />
             </a>
         </div>
-    </div>
+        </Col>
+        <Col>
+        <div className="footer">
+        <a href="https://www.linkedin.com/in/haley-telega-b6134712a/?originalSubdomain=ca" target="_blank" rel="noreferrer">
+            <img style={{ width: "10%" }}
+            src={require(`../../assets/footer/1.jpg`).default}
+            alt={'LinkedIn logo'}
+            className="footer-img2"
+            key={'LinkedIn logo'}
+            />
+            </a>
+        </div>
+        </Col>
+    </Row>
     </footer>
     );
 }
