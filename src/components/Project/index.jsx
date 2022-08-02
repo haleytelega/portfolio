5fd834658a935c996af660e1db652772160202fd
Haley Telega <haleytelega@outlook.com>
2022-08-02 15:59:32 -0400

    added finishing styles

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -17,27 +17,28 @@ function Projects () {
                 className="d-block w-100"
                 src={WeatherDashboard}
                 alt="Weather Dashboard"
-                width={400} height={400}
             />
-            <Carousel.Caption className="carousel-caption">
-                <h3>Weather Dashboard</h3>
-                <a className="projects githubproject1" href="https://github.com/haleytelega/Weather-Dashboard" target="_blank" rel="noreferrer">GitHub Weather Dashboard</a>
-                <a className="projects" href="https://haleytelega.github.io/Weather-Dashboard/" target="_blank" rel="noreferrer">Deployed Link Weather Dashboard</a>
+            <Carousel.Caption className="project1">
+                <p className="projects">Website where you can check for your daily and weekly weather.</p> 
+                <p className="projects"> Languages: HTML, CSS, Third-party API </p>
+                <a className="githubproject" href="https://github.com/haleytelega/Weather-Dashboard" target="_blank" rel="noreferrer">GitHub</a>
+                <a className="deployedprojects" href="https://haleytelega.github.io/Weather-Dashboard/" target="_blank" rel="noreferrer">Deployed Link</a>
             </Carousel.Caption>
         </Carousel.Item>
+
 
         <Carousel.Item>
             <img
                 className="d-block w-100"
                 src={WorkdayScheduler}
                 alt="Workday Scheduler"
-                width={800} height={800}
             />
 
-            <Carousel.Caption>
-                <h3>Workday Scheduler</h3>
-                <a className="projects githubproject2" href="https://github.com/haleytelega/Work-Day-Scheduler-" target="_blank" rel="noreferrer">GitHub Workday Scheduler</a>
-                <a className="projects" href="https://haleytelega.github.io/Work-Day-Scheduler-/" target="_blank" rel="noreferrer">Deployed Link Workday Scheduler</a>
+            <Carousel.Caption className="project1">
+                <p className="projects">Website where you can schedule your workday by hour</p> 
+                <p className="projects"> Languages: jQuery and Moment.js </p>
+                <a className="githubproject" href="https://github.com/haleytelega/Work-Day-Scheduler-" target="_blank" rel="noreferrer">GitHub</a>
+                <a className="deployedprojects" href="https://haleytelega.github.io/Work-Day-Scheduler-/" target="_blank" rel="noreferrer">Deployed Link</a>
             </Carousel.Caption>
         </Carousel.Item>
 
@@ -46,13 +47,13 @@ function Projects () {
                 className="d-block w-100"
                 src={PasswordGenerator}
                 alt="Password Generator"
-                width={800} height={800}
             />
 
-            <Carousel.Caption>
-                <h3>Password Generator</h3>
-                <a className="projects githubproject3" href="https://github.com/haleytelega/Password-Generator" target="_blank" rel="noreferrer">GitHub Password Generator</a>
-                <a className="projects" href="https://haleytelega.github.io/Password-Generator/" target="_blank" rel="noreferrer">Deployed Link Password Generator</a>
+            <Carousel.Caption className="project1">
+                <p className="projects">Website for a password generator</p> 
+                <p className="projects"> Languages: JavaScript </p>
+                <a className="githubproject" href="https://github.com/haleytelega/Password-Generator" target="_blank" rel="noreferrer">GitHub</a>
+                <a className="deployedprojects" href="https://haleytelega.github.io/Password-Generator/" target="_blank" rel="noreferrer">Deployed Link</a>
             </Carousel.Caption>
         </Carousel.Item>
 
@@ -61,13 +62,13 @@ function Projects () {
                 className="d-block w-100"
                 src={NoteTaker}
                 alt="Note Taker"
-                width={800} height={800}
             />
 
-            <Carousel.Caption>
-                <h3>Note Taker</h3>
-                <a className="projects githubproject4" href="https://github.com/haleytelega/Note-Taker" target="_blank" rel="noreferrer">GitHub Note Taker</a>
-                <a className="projects" href="https://ht-note-taker.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link Note Taker</a>
+            <Carousel.Caption className="project1">
+                <p className="projects">Website to take notes and save them for a later day</p> 
+                <p className="projects"> Languages: Express.js and JSON </p>
+                <a className="githubproject" href="https://github.com/haleytelega/Note-Taker" target="_blank" rel="noreferrer">GitHub</a>
+                <a className="deployedprojects" href="https://ht-note-taker.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link</a>
             </Carousel.Caption>
         </Carousel.Item>
 
@@ -76,12 +77,12 @@ function Projects () {
                 className="d-block w-100"
                 src={EmployeeTracker}
                 alt="Employee Tracker"
-                width={800} height={800}
             />
 
-            <Carousel.Caption>
-                <h3>Employee Tracker</h3>
-                <a className="projects" href="https://github.com/haleytelega/Employee-Tracker" target="_blank" rel="noreferrer">GitHub Employee Tracker</a>
+            <Carousel.Caption className="project1">
+                <p className="projects">Website to create a team profile</p> 
+                <p className="projects"> Languages/Frameworks: Node.js, OOP, TDD, Inquirer, Jest</p>
+                <a className="githubproject" href="https://github.com/haleytelega/Object-Oriented-Programming-Challenge-Team-Profile-Generator" target="_blank" rel="noreferrer">GitHub</a>
             </Carousel.Caption>
         </Carousel.Item>
 
@@ -90,13 +91,13 @@ function Projects () {
                 className="d-block w-100"
                 src={TechBlog}
                 alt="Tech Blog"
-                width={800} height={800}
             />
 
-            <Carousel.Caption>
-                <h3>Tech Blog</h3>
-                <a className="projects githubproject5" href="https://github.com/haleytelega/Tech-Blog" target="_blank" rel="noreferrer">GitHub Tech Blog</a>
-                <a className="projects" href="https://github.com/haleytelega/Tech-Blog" target="_blank" rel="noreferrer">Deployed Link Tech Blog</a>
+            <Carousel.Caption className="project1">
+                <p className="projects">Tech Blog for developers to post blogs and comment on others</p> 
+                <p className="projects"> Languages/Frameworks: Handlebars, Sequelize and Express.js </p>
+                <a className="githubproject" href="https://github.com/haleytelega/Tech-Blog" target="_blank" rel="noreferrer">GitHub</a>
+                <a className="deployedprojects" href="https://haleys-tech-blog.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link</a>
             </Carousel.Caption>
         </Carousel.Item>
 
